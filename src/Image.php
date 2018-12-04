1d8f7304cf6466e07fd57b0eacdb1eeb5254799e
Māris V <vilskersts.maris@gmail.com>
2018-12-04 16:49:22 +0200

    Added image trait for models

---
+++ src/Image.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Phycon\Larmin;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Phycon\Larmin\Utilities\UploadedImage\UploadedImage;
+
+trait Image
+{
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function setAttribute( $key, $value )
+    {
+        if( array_key_exists( $key, $this->imageFields ) )
+        {
+            $this->uploadImage( $key, $value );
+        }
+        else
+        {
+            parent::setAttribute( $key, $value );
+        }
+    }
+
+    /**
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     */
+    public function __call( $method, $parameters )
+    {
+        if( $method === 'image' )
+        {
+            return $this->imagePath( ...$parameters );
+        }
+
+        if (in_array($method, ['increment', 'decrement'])) {
+            return $this->$method(...$parameters);
+        }
+
+        return $this->forwardCallTo($this->newQuery(), $method, $parameters);
+    }
+
+    /**
+     * @param string $key
+     * @param string|null $preset
+     * @return string
+     */
+    protected function imagePath( $key, $preset = null )
+    {
+        if( $preset )
+        {
+            $path = $this->{$key};
+            $pathInfo = pathinfo( $path );
+            $presetFilePath = sprintf( '%s/%s_%s', $pathInfo['dirname'], $preset, $pathInfo['basename'] );
+
+            return Storage::url( $presetFilePath );
+        }
+
+        return Storage::url( $this->{$key} );
+    }
+
+    /**
+     * @param string $fieldName
+     * @param UploadedFile|null $image
+     */
+    protected function uploadImage( $fieldName, $image )
+    {
+        $uploadedImage = ( new UploadedImage( $image, self::class, $this->imageFields[$fieldName] ) )->save();
+
+        if( $uploadedImage->isSaved() )
+        {
+            parent::setAttribute( $fieldName, $uploadedImage->path() );
+        }
+        else
+        {
+            parent::setAttribute( $fieldName, null );
+        }
+    }
+}
---
+++ src/Utilities/UploadedImage/UploadedImage.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Phycon\Larmin\Utilities\UploadedImage;
+
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Illuminate\Http\UploadedFile;
+use Intervention\Image\Facades\Image as Imagick;
+
+class UploadedImage
+{
+    /**
+     * @var string|null
+     */
+    protected $model;
+
+    /**
+     * @var string|null
+     */
+    protected $path;
+
+    /**
+     * @var UploadedFile|null
+     */
+    protected $image;
+
+    /**
+     * @var array
+     */
+    protected $presets = [];
+
+    /**
+     * @var bool
+     */
+    protected $saved = false;
+
+    /**
+     * Image constructor.
+     * @param UploadedFile $image
+     * @param string $model
+     * @param array $presets
+     */
+    public function __construct( $image, $model, $presets )
+    {
+        $this->image = $image;
+        $this->model = $model;
+        $this->presets = $presets;
+    }
+
+    /**
+     * @return UploadedImage
+     */
+    public function save()
+    {
+        try
+        {
+            $fileName = $this->generateFileName();
+            $modelPath = $this->modelPath();
+
+            $this->path = $this->image->storeAs( $modelPath, $fileName );
+
+            foreach( $this->presets as $presetName => $presetOptions )
+            {
+                $width = $presetOptions[0];
+                $height = $presetOptions[1];
+
+                $canvas = Imagick::canvas( $width, $height );
+
+                if( $width >= $height )
+                {
+                    $resizedImage = Imagick::make( $this->image->getRealPath() )
+                        ->resize( $width, null, function ( $constraint ) {
+                            $constraint->aspectRatio();
+                        } )
+                        ->encode( $this->image->extension() );
+                }
+                else
+                {
+                    $resizedImage = Imagick::make( $this->image->getRealPath() )
+                        ->resize( null, $height, function ( $constraint ) {
+                            $constraint->aspectRatio();
+                        } )
+                        ->encode( $this->image->extension() );
+                }
+
+                $canvas->insert( $resizedImage, 'center' )->encode( $this->image->extension() );
+
+                Storage::put( $modelPath . '/' . $presetName . '_' . $fileName, $canvas->__toString() );
+            }
+        }
+        catch( FileException $exception )
+        {
+            return $this;
+        }
+
+        $this->saved = true;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    protected function modelPath()
+    {
+        return sprintf( 'public/%s', class_basename( $this->model ) );
+    }
+
+    /**
+     * @return string
+     */
+    protected function generateFileName()
+    {
+        return sprintf( '%s.%s', md5( file_get_contents( $this->image->getRealPath() ) ), $this->image->getClientOriginalExtension() );
+    }
+
+    /**
+     * @return null|string
+     */
+    public function path()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSaved()
+    {
+        return $this->saved;
+    }
+}
